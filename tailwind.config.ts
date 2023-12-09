bb6b0dfd1f8fd095ff6f4595302d2e71cb9c2c70
mohammedrakib944 <mohammedrakib944@gmail.com>
2023-12-09 18:24:50 +0600

    config done

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,32 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   plugins: [require("daisyui")],
-}
-export default config
+  daisyui: {
+    themes: [
+      // "blog_dark", // first one will be the default theme
+      {
+        blog_dark: {
+          primary: "#E78109",
+          secondary: "#DDCEC6",
+          accent: "#1e232a",
+          neutral: "#908A82",
+          "base-100": "#131E1F",
+          "base-200": "#0F1718  ",
+          info: "#58a6ff",
+          success: "#00d600",
+          warning: "#ef4444",
+          error: "#1e232a",
+        },
+        blog_light: {
+        },
+      },
+    ],
+  },
+};
+export default config;
