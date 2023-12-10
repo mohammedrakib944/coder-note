ade862ff4a042b9b85c8792f6f014f15097a6992
mohammedrakib944 <mohammedrakib944@gmail.com>
2023-12-10 22:10:11 +0600

    text color change

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,7 @@ const config: Config = {
           primary: "#d56405",
           secondary: "#414141",
           accent: "#eee4e4",
-          neutral: "#7f7c79",
+          neutral: "#66635f",
           // "base-100": "#131E1F",
           "base-100": "#FFFFFF",
           "base-200": "#fbfbfa",
